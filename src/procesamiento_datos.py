c5805c35d7c50600e7f8ce50ac704279e47758c9
brunoaruiz <ruizb@udesa.edu.ar>
2026-03-29 20:29:44 -0300

    file metricas y procesamientos_datos
    en procesamiento_datos esta la funcion de filtrar_por_participante, que con el ID busca el dicc con ese ID

---
+++ src/procesamiento_datos.py
@@ -0,0 +1,26 @@
+def filtar_por_participante(id, lista):
+    '''
+    A partir de un ID suministrado previamente,
+    esta función se encarga de buscar un diccionario
+    en una lista de dicc que tenga esa clave. Despues,
+    la función devuelve ese mismo dicc.
+
+    Parametros:
+    id: str
+    Es el id suministrado por el usuario.
+    lista: list
+    es la lista de diccionarios de cada participante
+
+    Returns:
+    dicc: dict
+    Es el diccionario que contiene el ID suministrado
+    None: NoneType
+    En caso de que el ID no se encuentre en la lista,
+    devuelve un None.
+    '''
+    for dicc in lista:
+        if id == dicc[id_participante]:
+            return dicc
+
+    return None
+
