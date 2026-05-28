663d3b6a26203a600007fc1b62a7c3eab414859a
mariajoseramirezo <mramirezosorio@udesa.edu.ar>
2026-05-28 14:26:39 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,3 +3,10 @@ import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt 
 
+st.title("Dashboard del MotionLAB")
+archivo = st.file_uploader("aca va el archivo", type= "csv") 
+
+if archivo is not None:
+    df = pd.read_csv(archivo)
+
+ 
